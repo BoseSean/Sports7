a0404a824ea47913c317a00fcc7f0af029cb5475
ambersyx <shiyuxin0324@gmail.com>
2017-11-06 17:17:54 +0800

    try push , need debug, added game and game player,added some more

--- app/src/main/java/org/team7/sports/CreateGameActivity.java
+++ app/src/main/java/org/team7/sports/CreateGameActivity.java
@@ -1,13 +1,131 @@
 package org.team7.sports;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.team7.sports.model.Game;
+import org.team7.sports.model.GamePlayer;
+
+import java.util.HashMap;
 
 public class CreateGameActivity extends AppCompatActivity {
+
+    private TextInputLayout mGameName;
+    private TextInputLayout mSportType;
+    private TextInputLayout mDate;
+    private TextInputLayout mStartTime;
+    private TextInputLayout mPasswd;
+    private TextInputLayout mNumberofppl;
+    private Button mSubmit;
+    private Button mCancel;
+    private CheckBox mPrivate;
+    private ProgressDialog RegisterProgress;
+    private TextInputLayout mLocation;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+    public void createGame(Game g) {
+        database = FirebaseDatabase.getInstance();
+        FirebaseUser currentUse = FirebaseAuth.getInstance().getCurrentUser();
+        g.setGameId(currentUse.getUid());
+        myRef = database.getReference().child("GameThread").child(g.getGameId());
+        HashMap<String, Game> hashmap = new HashMap<String, Game>();
+        hashmap.put(g.getGameName(), g);
+
+        myRef.setValue(hashmap).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    onBackPressed();
+                } else {
+                    Toast.makeText(CreateGameActivity.this, "failed to create", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_game);
+
+        mGameName = findViewById(R.id.gameName_TIL);
+        mSportType = findViewById(R.id.sportType_TIL);
+        mDate = findViewById(R.id.gameDate_TIL);
+        mStartTime = findViewById(R.id.startTime_TIL);
+        mPasswd = findViewById(R.id.private_password_TIL);
+        mNumberofppl = findViewById(R.id.number_TIL);
+        mSubmit = findViewById(R.id.game_create_submit_B);
+        mCancel = findViewById(R.id.game_create_cancel_B);
+        mPrivate = findViewById(R.id.privacy_check_box);
+        mLocation = findViewById(R.id.location_TIL);
+
+        RegisterProgress = new ProgressDialog(this);
+        mCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View view) {
+
+
+                String gameName = mGameName.getEditText().getText().toString();
+                String sportType = mSportType.getEditText().getText().toString();
+                int numberOfppl = Integer.parseInt(mNumberofppl.getEditText().getText().toString());
+                String passwd;
+                boolean isPrivate;
+                String date = mDate.getEditText().getText().toString();
+                String time = mStartTime.getEditText().getText().toString();
+                String location = mLocation.getEditText().getText().toString();
+
+                FirebaseUser currentUse = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = currentUse.getUid();
+                String email = currentUse.getEmail();
+                GamePlayer host = new GamePlayer(uid, email);
+                host.setIsHost();
+
+
+                if (mPrivate.isChecked()) {
+                    isPrivate = true;
+                    passwd = mPasswd.getEditText().getText().toString();
+                } else {
+                    isPrivate = false;
+                    passwd = "n";
+                }
+
+
+                Game g = new Game(passwd, isPrivate, gameName, sportType, location, date, time, numberOfppl, host);
+
+                if (!TextUtils.isEmpty((gameName)) || !TextUtils.isEmpty(sportType) || !TextUtils.isEmpty(mNumberofppl.getEditText().getText().toString())
+                        || !TextUtils.isEmpty(date) || !TextUtils.isEmpty(time)) {
+
+                    createGame(g);
+                }
+
+            }
+
+
+        });
     }
 }
--- app/src/main/java/org/team7/sports/ViewGameActivity.java
+++ app/src/main/java/org/team7/sports/ViewGameActivity.java
@@ -2,12 +2,80 @@ package org.team7.sports;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.team7.sports.model.Game;
+import org.team7.sports.model.GamePlayer;
 
 public class ViewGameActivity extends AppCompatActivity {
+
+    private TextView mGameName;
+    private TextView mSportType;
+    private TextView mDate;
+    private TextView mStartTime;
+    private TextView mLocation;
+    private TextView mHostName;
+    private TextView mHostEmail;
+    private String gameName;
+    private Game g;
+    private Button mJoinGame;
+    private String gameId;
+
+    private FirebaseDatabase database;
+    private DatabaseReference myref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_game);
+
+        gameId = getIntent().getStringExtra("this_game_id");
+        myref = FirebaseDatabase.getInstance().getReference().child("GameThread").child(gameId);
+        myref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                g = (Game) dataSnapshot.getValue();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mGameName.setText(gameName);
+        mSportType.setText(g.getSportType());
+        mDate.setText(g.getDate());
+        mStartTime.setText(g.getStartTime());
+        mLocation.setText(g.getLocation());
+        mHostName.setText(g.getHost().getUserId());
+        mHostEmail.setText(g.getHost().getEmail());
+
+        mJoinGame = findViewById(R.id.join_Game_B);
+        mJoinGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                FirebaseUser currentUse = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = currentUse.getUid();
+                String email = currentUse.getEmail();
+                GamePlayer p = new GamePlayer(uid, email);
+                g.addNewPlayer(p);
+                myref.setValue(p);
+            }
+        });
+
+
+
+
+
     }
 }
