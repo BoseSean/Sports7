05b5cae6e88aa65912acd14bf904679b3fd14cc4
BoseSean <xinyesg@gmail.com>
2017-11-06 17:13:11 +0800

    added activities

---
+++ app/src/main/java/org/team7/sports/ViewGameActivity.java
@@ -0,0 +1,13 @@
+package org.team7.sports;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_game);
+    }
+}
