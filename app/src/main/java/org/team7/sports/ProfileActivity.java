95c3d899f0f5324f5dc5102028a45f17981acece
BoseSean <xinyesg@gmail.com>
2017-11-11 13:42:35 +0800

    beautified profile activity

--- app/src/main/java/org/team7/sports/ProfileActivity.java
+++ app/src/main/java/org/team7/sports/ProfileActivity.java
@@ -2,7 +2,6 @@ package org.team7.sports;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,7 +14,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -33,7 +31,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile);
+        setContentView(R.layout.activity_profile);
 
         fUsername = findViewById(R.id.user_name);
         fFriendcount = findViewById(R.id.friend_count);
